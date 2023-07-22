56490f2e1cb9342b0c306d2f1bd9871672771f81
surya prakash <suryaprakashselvamani02@gmail.com>
2023-07-22 11:26:33 +0530

    developer added

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ def addition(a,b):
 a = int(input('enter the value of a: '))
 b = int(input('enter the value of b: '))
 addition(a,b)
+
+def additionb(a,b):
+    return a+b
+a = int(input('enter the value of a: '))
+b = int(input('enter the value of b: '))
+additionb(a,b)
